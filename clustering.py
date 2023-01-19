10e3ac21b8230589b8c48d8c8ce55dfd49ae3238
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2023-01-19 12:49:07 +0100

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,46 +1,79 @@
 import netCDF4 as nc
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import datetime as dt
-from read import warm_days_woy, warm_days_wod, get_heat_waves, values_hw, get_properties
-#from hw import HeatWave
-from utilities import mean_temp_wod, remove_leap_years, window_years_int, window_days, window_years, percentile, mean_temp, get_day, get_month, get_year, print_arr, get_wod
+import cftime
+from geop import strtodate,get_hw_days, distance, get_anomalies_map,HeatWave
 
-proj = ccrs.PlateCarree(central_longitude=180)
-proj0 = ccrs.PlateCarree(0)
-fn = '/home/giulia/Documents/Documenti/Tesi/pc_TAS_daymean_no29feb.nc'
-ds = nc.Dataset(fn)
-time = np.array(ds.variables['time'][:] - 438009.0)
-temp = np.array(ds.variables['t2m'][:])
-
-# tolgo ultimi 34 giorni
-index1 = np.arange(len(time)-34, len(time), 1)
-
-time = np.delete(time,  index1)
-temp = np.delete(temp, index1)
-dates = np.arange(dt.date(1950, 1, 1), dt.date(
-    2022, 1, 1), dtype='datetime64[D]')
+fn = '/home/giulia/geopotential_mean_no29feb.nc'
+ds = nc.Dataset(fn, 'r')
+lat = ds.variables['latitude']
+lat = np.array(lat[:])
+lon = np.array(ds.variables['longitude'][:])
+time = ds.variables['time']
+dates = cftime.num2pydate(time[:], time.units, calendar='standard')
+dates = pd.to_datetime(dates)
+dates = np.array(list(map(lambda x: x.date(), dates)))
 
 
-# rimuovo 29 febbraio da anni bisestili per semplificare matrice (72x365)
-dates = remove_leap_years(dates)
-temp = remove_leap_years(temp)
-time = remove_leap_years(time)
-date_matrix = np.reshape(dates, (int(len(dates)/365), 365))
-time_matrix = np.reshape(time, (int(len(time)/365), 365))
-temp_matrix = np.reshape(temp, (int(len(temp)/365), 365))
 
-print(len(temp_matrix))
-y=window_years(temp_matrix, 72)
-#print(y)
-print(y)
-print(len(y))
-#woy = warm_days_woy(temp_matrix, 60)
-#print(woy)
-#prov=get_heat_waves(woy)
-#print(prov)
+cluster_heat_waves=np.array([])
+cluster_heat_waves_anomalies=np.array([])
 
-#prop=get_properties(temp_matrix,prov)
-#print(prop)
+for i in range(0, 2):
+    df = pd.read_csv('/home/giulia/tesipy/properties_'+str(i+2017)+'.csv')
+    first_day = np.array(df['firstday'])
+    first_day = np.array(list(map(strtodate, first_day)))
+    first_day = np.array(list(map(lambda x: x.date(), first_day)))
+    duration = df['duration']
+    heat_waves_days = get_hw_days(dates, first_day, duration)
+    cluster_rs_hw = []
+
+
+    cluster_means_y = []
+    cluster_anomalies_means_y = []
+
+    for h in range(0,len(heat_waves_days)):
+        geop_maps_hw=[]
+        anomalies_maps_hw = []
+        for d in heat_waves_days[h]:
+            
+            geop_maps_hw.append(ds.variables['z'][d,:,:])               
+            anomalies_maps_hw.append(get_anomalies_map(ds, d, i))
+
+         
+        geop_maps_hw_np=np.array(geop_maps_hw)  
+        mean_geop=np.mean(geop_maps_hw_np,axis=0)    
+        cluster_means_y.append(mean_geop)
+        anomalies_maps_hw_np = np.array(anomalies_maps_hw)
+        anomalies_mean = np.mean(anomalies_maps_hw_np,axis=0)
+        cluster_anomalies_means_y.append(anomalies_mean) 
+
+        heat_wave_anomal=HeatWave([first_day[h]],[duration[h]],anomalies_mean) #se considero le anomalie nella mappa
+        heat_wave=HeatWave([first_day[h]],[duration[h]],mean_geop)  #se considero geopotenziale medio
+        
+        cluster_heat_waves=np.append(cluster_heat_waves,heat_wave)
+        cluster_heat_waves_anomalies=np.append(cluster_heat_waves_anomalies,heat_wave_anomal)
+
+
+
+matrix_distances=np.array([])
+for i in range(0,len(cluster_heat_waves)):
+    states=map(lambda x: x.geop_map, cluster_heat_waves)
+    distance_each_hw=[ distance(cluster_heat_waves[i].geop_map,x) for x in states]
+    matrix_distances=np.append(matrix_distances,distance_each_hw)
+    
+matrix_distances=np.reshape(matrix_distances,(len(cluster_heat_waves),len(cluster_heat_waves)))
+np.fill_diagonal(matrix_distances,0)
+d_min=np.min(matrix_distances[np.nonzero(matrix_distances)])
+positions=np.where(matrix_distances==d_min)
+
+merged_hw=cluster_heat_waves[positions[0][0]].merge(cluster_heat_waves[positions[0][1]])
+
+cluster_heat_waves=np.delete(cluster_heat_waves,(positions[0][0],positions[0][1]))
+cluster_heat_waves=np.append(cluster_heat_waves,merged_hw)
+
+print(len(cluster_heat_waves))
+print(matrix_distances)
+print(merged_hw.geop_map)
+
+
