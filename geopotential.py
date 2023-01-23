4299989ab58befb785f23076211bd7f84c2bae80
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2023-01-23 20:41:47 +0100

    Update geopotential.py

--- geopotential.py
+++ geopotential.py
@@ -8,19 +8,32 @@ def strtodate(day_str):
     result = datetime.strptime(day_str, '%Y-%m-%d').date()
     return pd.to_datetime(result)
 
+def strtodatetime(day_str):
+    result=datetime.strptime(day_str,'[datetime.date(%Y,%m,%d)]')
+    return pd.to_datetime(result)
 
+#def get_hw_days(dates, first_day, duration):
+#    begin = np.array(list(map(lambda x: np.where(dates == x), first_day)))
+#    indexes = []
+#    for i in range(0, len(duration)):
+#        indexes.append(np.arange(begin[i], begin[i]+1+duration[i], 1))
+#    return indexes
 def get_hw_days(dates, first_day, duration):
-    begin = np.array(list(map(lambda x: np.where(dates == x), first_day)))
+    begin = np.array([])
+    for x in first_day:
+        begin=np.append(begin,np.where(dates==x))
     indexes = []
-    for i in range(0, len(duration)):
+    #if len(duration!=0):
+    for i in range(0, len(begin)):
+    
         indexes.append(np.arange(begin[i], begin[i]+1+duration[i], 1))
     return indexes
 
 
+
 def r(event1, event2):
     num = np.sum(np.multiply(event1, event2))
-    den = math.sqrt(np.sum(np.multiply(event1, event1))) * \
-        math.sqrt(np.sum(np.multiply(event2, event2)))
+    den = math.sqrt(np.sum(np.multiply(event1, event1)))*math.sqrt(np.sum(np.multiply(event2, event2)))
     return num/den
 
 
@@ -29,9 +42,9 @@ def distance(event_map1, event_map2):
     return (1-coeff_corr)
 
 
-def window_days_int(year, day):
-    if (year*90+day) >= 440:
-        return np.arange(year*90+day-10, 450, 1)
+def window_days_int(dates,year, day):
+    if (year*90+day) >= len(dates)-5:
+        return np.arange(year*90+day-10, len(dates)-1, 1)
     if (year*90 + day) <= 10:
         return np.arange(0, day+11, 1)
     else:
@@ -41,48 +54,47 @@ def window_days_int(year, day):
 def woy_int(year):
     if(year <= 4):
         return np.arange(0, year+5, 1)
-    if(year >= 58):
-        return np.arange(year-5, 63, 1)  # controll range
+    if(year >= 55):
+        return np.arange(year-5, 60, 1)  
     else:
         return np.arange(year-5, year+5, 1)
 
 
-def wdy_int(years, day):
+def wdy_int(dates,years, day):
     days = np.array([])
     for year in years:
-        days = np.append(days, window_days_int(year, day))
+        days = np.append(days, window_days_int(dates,year, day))
     #lambda x: np.append(days,window_days_int(x,day)),years
     return days
 
 
-def day_mean(day_, year, ds):
-    days = wdy_int(woy_int(year), day_%90)
+def day_mean(dates,day_, year, ds):
+    days = wdy_int(dates,woy_int(year), day_%90)
     cluster_map = []
     for day in days:
-        cluster_map.append(np.array(ds.variables['z'][day, :, :]))
+        cluster_map.append(np.divide(np.array(ds.variables['z'][day, :, :]),9.80665))
     cluster_map_np = np.array(cluster_map)
     mean = np.mean(cluster_map_np, axis=0)
     return mean
 
-def day_percentile(day_, year, ds, p_value):
-    days = wdy_int(woy_int(year), day_%90)
+def day_percentile(dates,day_, year, ds, p_value):
+    days = wdy_int(dates,woy_int(year), day_%90)
     cluster_map = []
     for day in days:
-        cluster_map.append(np.array(ds.variables['z'][day, :, :]))
+        cluster_map.append(np.divide(np.array(ds.variables['z'][day, :, :]),9.80665))
     cluster_map_np = np.array(cluster_map)
     pct = np.percentile(cluster_map_np, p_value, axis=0)
     return pct
 
 
-def get_anomalies(ds, day_, year, p_value):
-    check_map = day_percentile(day_, year, ds, p_value) - \
-        ds.variables['z'][day_, :, :]
+def get_anomalies(dates,ds, day_, year, p_value):
+    check_map = day_percentile(dates,day_, year, ds, p_value) - np.divide(np.array(ds.variables['z'][day_, :, :]),9.80665)
     anomalies = np.where(check_map < 0)
     return anomalies
 
 
-def get_anomalies_map(ds, day_, year):
-    anomalies_map = day_mean(day_, year, ds)-ds.variables['z'][day_, :, :]
+def get_anomalies_map(dates,ds, day_, year):
+    anomalies_map = np.divide(np.array(ds.variables['z'][day_, :, :]),9.80665)-day_mean(dates,day_, year, ds)
     return anomalies_map
 
 
@@ -93,12 +105,13 @@ class HeatWave:
         self.first_day = first_day
         self.duration = duration
         self.geop_map = geop_map
+        self.cluster=[self]
 
-    def merge(self,other):
-        self.geop_map=np.mean(np.array([self.geop_map,other.geop_map]),axis=0)
-        self.first_day.append(other.first_day)
-        self.duration.append(other.duration)
-        return self
+    def add_hw(self, other):
+       self.cluster.append(self)
+       self.cluster.append(other)
+       return self.cluster
+
 
 def maxim_distance(cluster1,cluster2):
     arr_dist=np.array([])
@@ -118,7 +131,12 @@ def min_distance(cluster1,cluster2):
 
  
 
+ 
+
+
 # sulle righe=long
+
+
 
 
 
