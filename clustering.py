9da5b2c1d28f4912bdffbe459d4ac997995d5095
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2023-02-08 11:05:15 +0100

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import cftime
 import csv
-from datetime import datetime
-import datetime as dt
+#from datetime import datetime
+#import datetime as dt
 from geop import strtodate,get_hw_days, maxim_distance, get_anomalies_map,min_distance,HeatWave
 
 
@@ -12,11 +12,12 @@ def algorithm(cluster_heat_waves):
 
     matrix_distances=np.array([])
     for i in range(0,len(cluster_heat_waves)):
-        for hw in cluster_heat_waves:
-            distance_each_hw=[ maxim_distance(cluster_heat_waves[i],hw)]
+        for j in range(0,len(cluster_heat_waves)):
+            distance_each_hw= maxim_distance(cluster_heat_waves[i],cluster_heat_waves[j])
             matrix_distances=np.append(matrix_distances,distance_each_hw)
 
     matrix_distances=np.reshape(matrix_distances,(len(cluster_heat_waves),len(cluster_heat_waves)))
+    #print(matrix_distances)
     np.fill_diagonal(matrix_distances,0)
     d_min=np.min(matrix_distances[np.nonzero(matrix_distances)])
     positions=np.where(matrix_distances==d_min)
@@ -28,89 +29,118 @@ def algorithm(cluster_heat_waves):
     return cluster_heat_waves
 
 
-fn = '/home/giulia/geopotential_no29feb_f.nc'
+fn = '/home/giulia/geopotential_m.nc'
 ds = nc.Dataset(fn)
-lat = ds.variables['latitude']
-lat = np.array(lat[:])
-lon = np.array(ds.variables['longitude'][:])
 time = ds.variables['time']
 dates = cftime.num2pydate(time[:], time.units, calendar='standard')
 dates = pd.to_datetime(dates)
 dates = np.array(list(map(lambda x: x.date(), dates)))
-index=np.arange(0,14,1)
-dates=np.delete(dates,index)
+months = np.array(list(map(lambda x: x.month, dates)))
+days = np.array(list(map(lambda x: x.day, dates)))
+leap=[]
+for i in range(0,len(months)):
+    if months[i]==2 and days[i]==29:
+        leap.append(int(i))
+
+dates=np.delete(dates,leap)    
 
 cluster_heat_waves=[]
 cluster_heat_waves_anomalies=[]
-
-for i in range(0, 60):
-    df = pd.read_csv('/home/giulia/tesipy/properties_'+str(i+1959)+'.csv')
+heat_waves_days=[]
+first_days=[]
+durations=[]
+magnitudos=[]
+#ho cambiato il range
+for i in range(0, 61):
+    df = pd.read_csv('/home/giulia/tesipy2/JFM_90pct_'+str(i+1959)+'.csv')
     first_day = np.array(df['firstday'])
     first_day = np.array(list(map(strtodate, first_day)))
     first_day = np.array(list(map(lambda x: x.date(), first_day)))
-    duration = df['duration']
-    heat_waves_days = get_hw_days(dates, first_day, duration)
-    cluster_rs_hw = []
+    first_days.append(first_day)
+    duration = np.array(df['duration'])
+    durations.append(duration)
+    magnitudo=np.array(df['magnitudo'])
+    magnitudos.append(magnitudo)
 
+    heat_waves_days.append(get_hw_days(dates, first_day, duration))
 
-    cluster_anomalies_means_y = []
-    for h in range(0,len(heat_waves_days)):
-        anomalies_maps_hw = []
-        for d in heat_waves_days[h]:              
-            anomalies_maps_hw.append(np.divide(get_anomalies_map(dates,ds, d, i),9.80665) ) #divido per g dato da ECMWF
-        anomalies_maps_hw_np = np.array(anomalies_maps_hw)
-        anomalies_mean = np.mean(anomalies_maps_hw_np,axis=0)
-        cluster_anomalies_means_y.append(anomalies_mean) 
-        heat_wave_anomal=[HeatWave([first_day[h]],[duration[h]],anomalies_mean)] #se considero le anomalie nella mappa
-        cluster_heat_waves_anomalies.append(heat_wave_anomal)
-
-
-while len(cluster_heat_waves_anomalies)>7:
+   # print(heat_waves_days)
+cluster_rs_hw = []
+cluster_anomalies_means_y = []
+for h in range(0,len(heat_waves_days)):
+    
+    if len(heat_waves_days[h])!= 0:
+        for hw in range(0,len(heat_waves_days[h])):
+            anomalies_maps_hw = []
+            for d in heat_waves_days[h][hw]:              
+                anomalies_maps_hw.append(get_anomalies_map(dates,ds, d, int(d/90)) ) #divido per g dato da ECMWF
+            anomalies_maps_hw_np = np.array(anomalies_maps_hw)
+            anomalies_mean = np.mean(anomalies_maps_hw_np,axis=0)
+            cluster_anomalies_means_y.append(anomalies_mean) 
+            heat_wave_anomal=[HeatWave([first_days[h][hw]],[durations[h][hw]],[magnitudos[h][hw]],anomalies_mean)] #se considero le anomalie nella mappa
+            cluster_heat_waves_anomalies.append(heat_wave_anomal)
+print(cluster_heat_waves_anomalies)
+print(len(cluster_heat_waves_anomalies))
+#cambiato
+while len(cluster_heat_waves_anomalies)>3:
+    cluster_heat_waves_anomalies=algorithm(cluster_heat_waves_anomalies)
+#print(len(cluster_heat_waves_anomalies[0]),len(cluster_heat_waves_anomalies[1]),len(cluster_heat_waves_anomalies[2]))
+##print(cluster_heat_waves_anomalies)
+#print(len(cluster_heat_waves_anomalies))
+#
+##numero opportuno di clusters
+#d=np.array([])
+#num_cl=np.array([])
+#while len(cluster_heat_waves_anomalies)>=2:
+#    dist=np.array([])
+#    for i in range(0,len(cluster_heat_waves_anomalies)):
+#        
+#        for j in range(0,len(cluster_heat_waves_anomalies)):
+#            if(j==i):
+#                dist=np.append(dist,0) #modo rapido e brutto per non considerare interazioni con se stesso ma da eliminare
+#            else:
+#                distance_each_hw=[ maxim_distance(cluster_heat_waves_anomalies[i],cluster_heat_waves_anomalies[j])]
+#                dist=np.append(dist,distance_each_hw)
+#    min=np.min(dist[np.nonzero(dist)]) 
+#    d=np.append(d,min)
+#    num_cl=np.append(num_cl,len(cluster_heat_waves_anomalies))
+#
+#    cluster_heat_waves_anomalies=algorithm(cluster_heat_waves_anomalies)
+#
+#print('d: ',d)
+#print('num_cl: ',num_cl)
+#
+#delta_d=np.array([])
+#for i in range(0,len(d)-1):
+#    delta_d=np.append(delta_d,d[i+1]-d[i])
+#    print(str(i+1)+'-'+str(i))
+#print('delta_d= ', delta_d)
+#max_delta_d=np.max(delta_d)
+#n=int(np.where(delta_d==max_delta_d)[0])
+#5 e non 6 perchè i np.array partono da 0
+for i in range(0,3):
     cluster_heat_waves_anomalies=algorithm(cluster_heat_waves_anomalies)
 
 
-##numero opportuno di cluster
-cluster_check=cluster_heat_waves_anomalies.copy()
-d=np.array([])
-
-while len(cluster_check)>=2:
-    dist=np.array([])
-    for i in range(0,len(cluster_check)):
-        
-        for j in range(0,len(cluster_check)):
-            if(j==i):
-                dist=np.append(dist,10) #modo rapido e brutto per non considerare interazioni con se stesso ma da eliminare
-            else:
-                distance_each_hw=[ maxim_distance(cluster_check[i],cluster_check[j])]
-                dist=np.append(dist,distance_each_hw)
-    min=np.min(dist[np.nonzero(dist)]) 
-    d=np.append(d,min)
-    cluster_check=algorithm(cluster_check)
-
-
-delta_d=np.array([])
-for i in range(0,len(d)-1):
-    delta_d=np.append(delta_d,d[i+1]-d[i])
-max_delta_d=np.max(delta_d)
-n=int(np.where(delta_d==max_delta_d)[0])
-
-i=6-n #5 e non 6 perchè i np.array partono da 0
-for i in range(6-n,7):
-    cluster_heat_waves_anomalies=algorithm(cluster_heat_waves_anomalies)
 
 
 for j in range(0,len(cluster_heat_waves_anomalies)):
-    with open("cluster_geop_fd"+str(j)+".csv", "w") as stream:
+    with open("JFM_90pct_fd"+str(j)+".csv", "w") as stream:
         writer = csv.writer(stream)
         writer.writerow(['firstday'])
         for hw in cluster_heat_waves_anomalies[j]:      
             writer.writerow(hw.first_day)
         stream.close()
-    with open("cluster_geop_dur"+str(j)+".csv", "w") as stream:
+    with open("JFM_90pct_dur"+str(j)+".csv", "w") as stream:
         writer = csv.writer(stream)
         writer.writerow(['duration'])
         for hw in cluster_heat_waves_anomalies[j]:        
             writer.writerow(hw.duration)
         stream.close()
-
+    with open("JFM_90pct_mag"+str(j)+".csv", "w") as stream:
+        writer = csv.writer(stream)
+        writer.writerow(['magnitudo'])
+        for hw in cluster_heat_waves_anomalies[j]:        
+            writer.writerow(hw.magnitudo)
+    stream.close()
 
