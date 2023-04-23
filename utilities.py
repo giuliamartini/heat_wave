39e9ce5ab92c25018d6a1944ba9bd5a02d67a758
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2023-04-23 18:02:04 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,14 +1,15 @@
+
 import numpy as np
 
 
-def window_years_int(year):
+def window_years_int(year,temp_matrix):
 
     if year in range(0, 4):
         result = np.arange(0, year+5)
         return result
 
-    if year in range(61-4, 61):
-        result = np.arange(year-4, 61)
+    if year in range(len(temp_matrix)-4, len(temp_matrix)-1):
+        result = np.arange(year-4, len(temp_matrix)-1)
         return result
     else:
         result = np.arange(year-4, year+5)
@@ -26,21 +27,21 @@ def window_days(temp_matrix, year, day):
         index = np.arange(0, 10-day)
         previous_year = np.array([])
         previous_year = np.append(
-            previous_year, temp_matrix[year-1, 355+day+index])
+            previous_year, temp_matrix[year-1, len(temp_matrix[0])-10+day+index])
         index1 = np.arange(0, 11)
         previous_year = np.append(previous_year, temp_matrix[year, index1])
         return previous_year
 
-    if day in range(355, 365) and year == 61:
+    if day in range(len(temp_matrix[0])-10, len(temp_matrix[0])) and year == len(temp_matrix)-1:
         if(day > 366):
             raise Exception("day is out of range")
         index = np.arange(day-10, 365)
         return temp_matrix[year, index]
-    if day in range(355, 365) and year != 61:
+    if day in range(len(temp_matrix[0])-10, len(temp_matrix[0])) and year != len(temp_matrix)-1:
         index = np.arange(day-10, 365)
         next_year = np.array([])
         next_year = np.append(next_year, temp_matrix[year, index])
-        index1 = np.arange(0, 10-(365-day))
+        index1 = np.arange(0, 10-(len(temp_matrix[0])-day))
         next_year = np.append(next_year, temp_matrix[year+1, index1])
         return next_year
     else:
@@ -54,10 +55,10 @@ def window_years(temp_matrix, year):
     if year in range(0, 4):
         return temp_matrix[0:year+4]
 
-    if year in range(62-4, 62):
-        return temp_matrix[(year-4):62]
+    if year in range(len(temp_matrix)-4, len(temp_matrix)-1):
+        return temp_matrix[(year-4):len(temp_matrix)-1]
 
-    if year > 62:
+    if year > len(temp_matrix)-1:
         raise Exception("year is out of range")
     else:
         return temp_matrix[(year-4):(year+4)]
@@ -66,9 +67,9 @@ def window_years(temp_matrix, year):
 def percentile_d(temp_matrix, year, p_val):
     percentile_each_day = np.array([])
 
-    years = window_years_int(year)
+    years = window_years_int(year, temp_matrix)
 
-    for i in range(0, 365):
+    for i in range(0, len(temp_matrix[0])):
         temp_wod = np.array([])
 
         for j in range(0, len(years)):
@@ -82,12 +83,12 @@ def percentile_d(temp_matrix, year, p_val):
 
 
 def mean_temp_day(temp_matrix, year, day):
-    if year > 72 or year < 0:
+    if year > len(temp_matrix)-1 or year < 0:
         raise Exception("year is out of range")
-    if day > 365 or day < 0:
+    if day > len(temp_matrix[0]) or day < 0:
         raise Exception("day is out of range")
     temps_each_year = np.array([])
-    years = window_years_int(year)
+    years = window_years_int(year, temp_matrix)
     for j in range(0, len(years)):
         temp = window_days(temp_matrix, years[j], day)
         temps_each_year = np.append(temps_each_year, temp)
